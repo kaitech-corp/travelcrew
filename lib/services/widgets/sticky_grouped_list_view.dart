9a47f9d70ee9484fa2fa839e3d9c82618e71f87f
randynolden <randy@kaitechcorp.com>
2021-09-09 17:11:04 -0700

    Removed dispose from blocs because they are needed throught the life-cycle. Added Grouped List view for my crew trips current, past, private. Moved private to end of tab bar. Changed date time display for chats. Added date time functions for grouped list view. Separated trip repository to one for each. Fixed chat timestamp issue by changing default to timestamp.now().

---
+++ lib/services/widgets/sticky_grouped_list_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:sticky_grouped_list/sticky_grouped_list.dart';
+import 'package:travelcrew/models/chat_model.dart';
+import 'package:travelcrew/screens/trip_details/chat/chat_card.dart';
+import 'package:travelcrew/services/widgets/chat_date_display.dart';
+
+class StickyGroupedChatListView extends StatelessWidget {
+  const StickyGroupedChatListView({
+    Key key,
+    @required this.data,
+    @required this.documentId
+  }) : super(key: key);
+
+  final dynamic data;
+  final String documentId;
+
+  @override
+  Widget build(BuildContext context) {
+    return StickyGroupedListView<ChatData, String>(
+      key: Key(data.length.toString()),
+      elements: data,
+      reverse: true,
+      groupBy: (chat)
+      {
+        return DateTime(chat.timestamp.toDate().year, chat.timestamp.toDate().month,
+            chat.timestamp.toDate().day, chat.timestamp.toDate().hour)
+            .toString();
+      },
+      order: StickyGroupedListOrder.DESC,
+      itemComparator:(a,b) => a.timestamp.compareTo(b.timestamp),
+      groupSeparatorBuilder: (chat) =>
+          Padding(
+              padding: const EdgeInsets.all(4.0),
+              child:ChatDateDisplay(dateString: chat.timestamp.toDate().toString(),)),
+      itemBuilder: (context, chat){
+        return ChatCard(message: chat, tripDocID: documentId,);
+      },
+    );
+  }
+}
