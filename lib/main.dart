7c2a96ce04b60d63255ac87dc447e3f09c18fcef
randynolden <randy@kaitechcorp.com>
2021-11-12 11:24:06 -0800

    Latest Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,25 @@ import 'dart:io';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:firebase_performance/firebase_performance.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:responsive_framework/responsive_framework.dart';
 import 'package:sizer/sizer.dart';
 import 'package:travelcrew/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:travelcrew/blocs/authentication_bloc/authentication_event.dart';
 import 'package:travelcrew/blocs/authentication_bloc/authentication_state.dart';
-import 'package:travelcrew/blocs/bloc_observer/custom_bloc_observer.dart';
 import 'package:travelcrew/repositories/user_repository.dart';
 import 'package:travelcrew/screens/complete_profile/complete_profile_page.dart';
 import 'package:travelcrew/screens/login/login_screen.dart';
 import 'package:travelcrew/services/database.dart';
 import 'package:travelcrew/services/firebase_messaging.dart';
+import 'package:travelcrew/services/initializer/project_initializer.dart';
 import 'package:travelcrew/services/locator.dart';
 import 'package:travelcrew/services/navigation/navigation_service.dart';
 import 'package:travelcrew/services/navigation/router.dart';
+import 'package:travelcrew/services/responsive/responsive_wrapper.dart';
+import 'package:travelcrew/services/theme/theme_data.dart';
 import 'package:travelcrew/services/widgets/launch_icon_badger.dart';
 import 'package:travelcrew/services/widgets/loading.dart';
 import 'package:travelcrew/size_config/size_config.dart';
@@ -33,21 +30,9 @@ import 'package:travelcrew/size_config/size_config.dart';
 
 void main() async {
 
-  WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-  await Firebase.initializeApp();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown
-  ]);
+  await ProjectInitializer();
 
-  Bloc.observer = CustomBlocObserver();
   final UserRepository userRepository = UserRepository();
-
-  // Pass all uncaught errors from the framework to Crashlytics.
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-  FirebasePerformance.instance.setPerformanceCollectionEnabled(true);
-  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
 
   runApp(BlocProvider(
     create: (BuildContext context) => AuthenticationBloc(
@@ -70,10 +55,8 @@ class _TravelCrewState extends State<TravelCrew> {
 
   @override
   void initState() {
-    DatabaseService().getUserNotificationSettings();
-    super.initState();
+    // DatabaseService().getUserNotificationSettings();
 
-    setupLocator();
     if (Platform.isIOS) {
       FBMessaging().requestPermissions();
     } else {
@@ -82,6 +65,7 @@ class _TravelCrewState extends State<TravelCrew> {
         FBMessaging().firebaseMessagingBackgroundHandler(message);
       });
     }
+    super.initState();
   }
   @override
   Widget build(BuildContext context) {
@@ -90,18 +74,7 @@ class _TravelCrewState extends State<TravelCrew> {
       builder: (context,orientation, deviceType) {
         return MaterialApp(
                 builder: (context, widget) {
-                  return ResponsiveWrapper.builder(
-                    BouncingScrollWrapper.builder(context, widget),
-                    maxWidth: 1200,
-                    minWidth: 400,
-                    defaultScale: true,
-                    breakpoints: [
-                      ResponsiveBreakpoint.resize(400, name: MOBILE),
-                      ResponsiveBreakpoint.autoScale(800, name: TABLET),
-                      ResponsiveBreakpoint.resize(1000, name: DESKTOP),
-                    ],
-                    // background: Container(color: Color(0xFFF5F5F5))
-                  );
+                  return ResponsiveWrapperBuilder(context, widget);
                 },
                 home: BlocBuilder<AuthenticationBloc,AuthenticationState>(
                         builder: (context,state){
@@ -127,102 +100,7 @@ class _TravelCrewState extends State<TravelCrew> {
                           }
                         }),
                 debugShowCheckedModeBanner: false,
-                theme:  ThemeData(
-                  fontFamily: 'Cantata One',
-                  textTheme: TextTheme(
-                    headline1: const TextStyle(fontWeight: FontWeight.bold,
-                        color: Colors.black),
-                    headline2: const TextStyle(fontWeight: FontWeight.bold,
-                        color: Colors.black),
-                    headline3: const TextStyle(fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                        fontStyle: FontStyle.italic),
-                    headline4: const TextStyle(fontWeight: FontWeight.bold,
-                        color: Colors.black),
-                    headline5: const TextStyle(fontWeight: FontWeight.bold,
-                      color: Colors.black,),
-                    headline6: const TextStyle(fontWeight: FontWeight.bold,
-                    ),
-                    subtitle1: const TextStyle(fontWeight: FontWeight.bold),
-                    subtitle2: const TextStyle(fontWeight: FontWeight.w600, fontStyle: FontStyle.italic,),
-                    button: const TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.black),
-                  ),
-                  elevatedButtonTheme: ElevatedButtonThemeData(
-                    style: ButtonStyle(
-                        shape: MaterialStateProperty.all<
-                            RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            )
-                        ),
-                        textStyle: MaterialStateProperty.all<TextStyle>(
-                          TextStyle(fontFamily: 'Cantata One',
-                            fontWeight: FontWeight.bold,),
-                        ),
-                        foregroundColor: MaterialStateProperty.all<Color>(
-                            Colors.black
-                        )
-                    ),
-                  ),
-                  outlinedButtonTheme: OutlinedButtonThemeData(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          side: const BorderSide(color: Colors.grey),
-                        ),
-                      ),
-                      textStyle: MaterialStateProperty.all<TextStyle>(
-                        TextStyle(fontFamily: 'Cantata One',
-                          fontWeight: FontWeight.bold,),
-                      ),
-                    ),
-                  ),
-                  textButtonTheme: TextButtonThemeData(
-                    style: ButtonStyle(
-                      textStyle: MaterialStateProperty.all<TextStyle>(
-                        TextStyle(fontFamily: 'Cantata One',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.lightBlue),
-                      ),
-                    ),
-                  ),
-                  appBarTheme: AppBarTheme(
-                    iconTheme: IconThemeData(color: Colors.black),),
-                  primaryIconTheme: IconThemeData(
-                      size: SizerUtil.deviceType == DeviceType.tablet ? 36 : 24,
-                      color: Colors.black
-                  ),
-                  buttonColor: Colors.blue,
-                  floatingActionButtonTheme: FloatingActionButtonThemeData(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.blue
-                  ),
-                  bottomNavigationBarTheme: BottomNavigationBarThemeData(
-                      backgroundColor: Color(0xFF121212),
-                      selectedItemColor: Colors.blueAccent,
-                      unselectedItemColor: Color(0xFF121212)
-                  ),
-                  canvasColor: Color(0xFFFAFAFA),
-                  accentIconTheme: IconThemeData(
-                      color: Colors.black
-                  ),
-                  brightness: Brightness.light,
-                  primaryColor: Colors.white,
-                  accentColor: Colors.blueAccent,
-                  inputDecorationTheme: InputDecorationTheme(
-                    labelStyle: TextStyle(color: Colors.black),
-                    fillColor: Colors.grey[300],
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(color: Colors.black),
-                    ),
-                  ),
-                  iconTheme: IconThemeData(
-                    color: Colors.white,
-                    size: SizerUtil.deviceType == DeviceType.tablet ? 36 : 24,
-                  ),
-                ),
+                theme:  ThemeDataBuilder(),
                 navigatorKey: locator<NavigationService>().navigationKey,
                 onGenerateRoute: generateRoute,
                 navigatorObservers: [
@@ -232,4 +110,6 @@ class _TravelCrewState extends State<TravelCrew> {
       }
     );
   }
+
+
 }
