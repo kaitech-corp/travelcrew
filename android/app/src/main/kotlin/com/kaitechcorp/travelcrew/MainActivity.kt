7b0c5e601724069f295cb740308ed12c85ffbc76
randynolden <randy@kaitechcorp.com>
2022-04-05 21:52:43 -0700

    Platform updates

--- android/app/src/main/kotlin/com/kaitechcorp/travelcrew/MainActivity.kt
+++ android/app/src/main/kotlin/com/kaitechcorp/travelcrew/MainActivity.kt
@@ -5,8 +5,15 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
+//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+//        .setMethodCallHandler(
+//                (call, result) -> {
+//
+//        }
+//        );
+//    }
 }
