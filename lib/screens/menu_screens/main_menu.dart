80e27abc576f6b19d827eea93ba7bde79cb7670c
randynolden <randy@kaitechcorp.com>
2022-04-20 13:44:33 -0700

    Added SMS Feature allowing users to invite friends

--- lib/screens/menu_screens/main_menu.dart
+++ lib/screens/menu_screens/main_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sizer/sizer.dart';
+import 'package:travelcrew/services/social/app_invite.dart';
 
 import '../../blocs/authentication_bloc/authentication_bloc.dart';
 import '../../blocs/authentication_bloc/authentication_event.dart';
@@ -135,6 +136,15 @@ class _MenuDrawerState extends State<MenuDrawer> {
                     navigationService.navigateTo(AdminPageRoute);
                 },
               ),
+            ListTile(
+              leading: const IconThemeWidget(
+                  icon: Icons.send),
+              title:
+              Text('Invite Friends', style: Theme.of(context).textTheme.subtitle1),
+              onTap: () {
+                AppInvite().sendInvite();
+              },
+            ),
             ListTile(
               leading: const IconThemeWidget(icon: Icons.exit_to_app),
               title:
