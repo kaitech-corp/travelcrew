f419effa22a0bbfcd829bbec70b5aa1f0d5ea979
randynolden <randy@kaitechcorp.com>
2022-12-29 05:56:09 -0800

    feat: new function

---
+++ lib/services/widgets/ai_widgets/functions.dart
@@ -0,0 +1,5 @@
+// int calculateDaysBetweenTimestamps(Timestamp dateCreatedTimestamp, Timestamp endDateTimestamp) {
+//   int difference = endDateTimestamp.toMilliseconds() - dateCreatedTimestamp.toMilliseconds();
+//   double days = difference / Duration.millisecondsPerDay;
+//   return days.round();
+// }
