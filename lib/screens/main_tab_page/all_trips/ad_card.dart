7f073debf5b673af5ef89a94ae5dec9ed828e933
randynolden <randy@kaitechcorp.com>
2022-12-29 05:43:37 -0800

    feat: Model change

--- lib/screens/main_tab_page/all_trips/ad_card.dart
+++ lib/screens/main_tab_page/all_trips/ad_card.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../../models/custom_objects.dart';
-import '../../../services/functions/cloud_functions.dart';
 import '../../../services/functions/tc_functions.dart';
+import '../../../services/widgets/link_previewer.dart';
 import '../../../size_config/size_config.dart';
 
 
@@ -11,30 +11,34 @@ class AdCard extends StatelessWidget{
 
   const AdCard({Key? key, required this.tripAds}) : super(key: key);
 
-  final TripAds tripAds;
+  final DestinationModel tripAds;
 
   @override
   Widget build(BuildContext context) {
 
     return InkWell(
-      key: Key(tripAds.documentID),
+      key: Key(tripAds.name!),
       splashColor: Colors.blue.withAlpha(30),
 
       child: GestureDetector(
         onTap: () {
-          TCFunctions().launchURL(tripAds.link);
-          CloudFunction().updateClicks(tripAds.documentID);
+          TCFunctions().launchURL(tripAds.url!);
+          // CloudFunction().updateClicks(tripAds.documentID);
         },
         child: Container (
           height: SizeConfig.screenWidth*.5,
           width: SizeConfig.screenWidth*.5,
           margin: const EdgeInsets.only(left: 15, right: 15, bottom: 20, top: 10),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(tripAds.urlToImage),
-              fit: BoxFit.fill,
-            ),
-            borderRadius: const BorderRadius.only(topLeft: Radius.circular(40), bottomRight: Radius.circular(40)),
+          decoration: const BoxDecoration(
+            // image: DecorationImage(
+            //   image:
+            //   fit: BoxFit.fill,
+            // ),
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(40), bottomRight: Radius.circular(40)),
+          ),
+          child: ViewAnyLink(
+            link: tripAds.url!,
+            function: () => <void>{},
           ),
         ),
       ),
--- lib/screens/main_tab_page/all_trips/layout_items/ad_list_us.dart
+++ lib/screens/main_tab_page/all_trips/layout_items/ad_list_us.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../../blocs/generics/generic_bloc.dart';
-import '../../../../blocs/generics/generic_state.dart';
 import '../../../../blocs/generics/generics_event.dart';
 import '../../../../models/custom_objects.dart';
 import '../../../../repositories/trip_ad_repository.dart';
-import '../../../../services/widgets/loading.dart';
+import '../../../../services/database.dart';
 import '../../../../size_config/size_config.dart';
 import '../ad_card.dart';
 
@@ -14,21 +13,20 @@ import '../ad_card.dart';
 class SliverGridAdList extends StatefulWidget {
   const SliverGridAdList({Key? key}) : super(key: key);
 
-
   @override
   State<SliverGridAdList> createState() => _SliverGridAdListState();
 }
 
 class _SliverGridAdListState extends State<SliverGridAdList> {
-
-  late GenericBloc<TripAds,TripAdRepository>  tripAdBloc;
+  late GenericBloc<TripAds, TripAdRepository> tripAdBloc;
 
   int crossAxisCount = 2;
 
   @override
   void initState() {
     super.initState();
-    tripAdBloc = BlocProvider.of<GenericBloc<TripAds,TripAdRepository>>(context);
+    tripAdBloc =
+        BlocProvider.of<GenericBloc<TripAds, TripAdRepository>>(context);
     tripAdBloc.add(LoadingGenericData());
   }
 
@@ -39,24 +37,50 @@ class _SliverGridAdListState extends State<SliverGridAdList> {
         crossAxisCount = 4;
       });
     }
-    return BlocBuilder<GenericBloc<TripAds,TripAdRepository>, GenericState>(
-        builder: (BuildContext context, GenericState state) {
-          if (state is LoadingState) {
-            return const Flexible(child: Loading());
-          } else if (state is HasDataState) {
-            final List<TripAds> adList = state.data as List<TripAds>;
+    return FutureBuilder<List<DestinationModel>>(
+        future: DatabaseService().getDestinations(),
+        builder: (BuildContext context,
+            AsyncSnapshot<List<DestinationModel>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else if (snapshot.hasData) {
+            List<DestinationModel> destinations = snapshot.data!;
+            print(destinations.length);
             return SizedBox(
-              height: SizeConfig.screenWidth*.55,
+              height: SizeConfig.screenWidth * .55,
               child: ListView(
                 scrollDirection: Axis.horizontal,
-                children: List<Widget>.generate(adList.length, (int index) {
-                  return AdCard(tripAds: adList[index]);
+                children:
+                    List<Widget>.generate(destinations.length, (int index) {
+                  return AdCard(tripAds: destinations[index]);
                 }),
               ),
             );
           } else {
-            return Container();
+            return const Text('No destinations found');
           }
+          // return BlocBuilder<GenericBloc<DestinationModel,DestinationRepository>, GenericState>(
+          //     builder: (BuildContext context, GenericState state) {
+          //       if (state is LoadingState) {
+          //         return const Flexible(child: Loading());
+          //       } else if (state is HasDataState) {
+          //         final List<DestinationModel> adList = state.data as List<DestinationModel>;
+          //         return SizedBox(
+          //           height: SizeConfig.screenWidth*.55,
+          //           child: ListView(
+          //             scrollDirection: Axis.horizontal,
+          //             children: List<Widget>.generate(adList.length, (int index) {
+          //               return AdCard(tripAds: adList[index]);
+          //             }),
+          //           ),
+          //         );
+          //       } else {
+          //         return Container();
+          //       }
+          //     });
+          // }
         });
   }
 }
