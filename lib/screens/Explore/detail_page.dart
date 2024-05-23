3426ec477ee5a892c8443d510848936327ac3b15
randynolden <rnolden2@gmail.com>
2024-05-23 10:52:18 -0700

    feat: explore updates

---
+++ lib/screens/Explore/detail_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+import '../../models/activity_model/activity_model.dart';
+import '../../models/lodging_model/lodging_model.dart';
+import '../../models/transportation_model/transportation_model.dart';
+import '../../models/trip_model/trip_model.dart';
+import '../Activities/activity_details.dart';
+import '../Lodging/lodging_details.dart';
+import '../Transportation/transportation_details.dart';
+
+
+class DetailsPage extends StatelessWidget{
+
+  const DetailsPage({super.key, required this.activity,this.lodging,required this.transport,required this.type, required this.trip});
+
+  final ActivityModel? activity;
+  final LodgingModel? lodging;
+  final TransportationModel? transport;
+  final Trip trip;
+  final String type;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    switch (type){
+      case 'Activity':
+        return ActivityDetails(activity: activity!,trip: trip,);
+      case 'Lodging':
+        return LodgingDetails(lodging:lodging!,trip: trip,);
+      default:
+        return TransportationDetails(transport:transport!,);
+    }
+  }
+
+}
