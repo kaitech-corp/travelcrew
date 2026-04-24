539b26de6e15b72538bb7ea896ecfecb329fd3c2
randynolden <rnolden2@gmail.com>
2026-04-23 20:37:37 -0700

    build: android

---
+++ android/app/src/main/kotlin/com/example/travelcrew/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.travelcrew
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/kaitechcorp/travel_crew/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kaitechcorp.travel_crew
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
